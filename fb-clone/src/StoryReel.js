e027807228a6ba5da5244cb38e41cc3780b0853b
Marion Richez <richez.m95@gmail.com>
2020-09-11 16:14:49 +0200

    ajout du content dans la partie story

---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Story from './Story'
+import './StoryReel.css'
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="John Doe"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sam Doe"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Will Doe"
+            />
+            <Story
+                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+                title="Sammy Doe"
+            />
+        </div>
+    )
+}
+
+export default StoryReel
