6a181e5d5dfb71a53baa59a85901f51e82598e6e
Marion Richez <richez.m95@gmail.com>
2020-09-11 16:12:57 +0200

    ajout de la config firebase

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAwTh_6wXJc2SQO3zpLUTtoQ0xsiXW5Wc",
+    authDomain: "facebook-clone-1a92e.firebaseapp.com",
+    databaseURL: "https://facebook-clone-1a92e.firebaseio.com",
+    projectId: "facebook-clone-1a92e",
+    storageBucket: "facebook-clone-1a92e.appspot.com",
+    messagingSenderId: "118899144685",
+    appId: "1:118899144685:web:00e4d120b722b8747519e4",
+    measurementId: "G-Q8LK03NE6V"
+};
