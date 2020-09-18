5463a0cc4555d31211ffa52e77ca64e8a952b0cf
Marion Richez <richez.m95@gmail.com>
2020-09-18 17:23:15 +0200

    modification des images

--- fb-clone/src/StoryReel.js
+++ fb-clone/src/StoryReel.js
@@ -6,29 +6,29 @@ function StoryReel() {
     return (
         <div className="storyReel">
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
-                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-                title="Sonny Sangha"
+                image="https://images.unsplash.com/photo-1568734066241-45094773addb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                profileSrc="https://images.unsplash.com/photo-1597096793378-0639088483e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+                title="Sheena Earl"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
-                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-                title="John Doe"
+                image="https://images.unsplash.com/photo-1582192330302-b24e8ad458b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"
+                profileSrc="https://images.unsplash.com/photo-1599986146464-d11377649c3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+                title="Van Darby"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
-                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-                title="Sam Doe"
+                image="https://images.unsplash.com/photo-1588208265940-80a7c7d1619d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                profileSrc="https://images.unsplash.com/photo-1599488981203-d107749d9feb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                title="Angus Saunders"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
-                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-                title="Will Doe"
+                image="https://images.unsplash.com/photo-1559294824-627e9738df81?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                profileSrc="https://images.unsplash.com/photo-1585282779872-6ec1b149e551?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+                title="Lewis Colton"
             />
             <Story
-                image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
-                profileSrc="https//avatars2.githubbusercontent.com/u/247129567s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
-                title="Sammy Doe"
+                image="https://images.unsplash.com/photo-1599824567234-b8bc5dbcd02e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80"
+                profileSrc="https://images.unsplash.com/photo-1598639908604-9d184deda456?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+                title="Tracy Evanson"
             />
         </div>
     )
